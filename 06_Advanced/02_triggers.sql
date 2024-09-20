509eebcbf2b88533614cdbfb46e26d2459e9efab
Santiago Gaviria <sgaviria@uc.edu.co>
2024-09-20 15:52:41 -0500

    Avanzado Modify

--- 06_Advanced/02_triggers.sql
+++ 06_Advanced/02_triggers.sql
@@ -22,11 +22,13 @@ CREATE TRIGGER tg_email
 AFTER UPDATE ON users
 FOR EACH ROW
 BEGIN
-	IF OLD.email <> NEW.email THEN
-		INSERT INTO email_history (user_id, email)
-		VALUES (OLD.user_id, OLD.email);
-	END IF;
-END//
+    IF OLD.email <> NEW.email THEN
+        INSERT INTO email_history (user_id, email)
+        VALUES (OLD.user_id, OLD.email);
+    END IF;
+END //
+
+DELIMITER ;
 
 -- Actualiza el campo "email" del usuario 1 la tabla "users" para probar el trigger
 UPDATE users SET email = 'mouredev@gmail.com' WHERE user_id = 1
