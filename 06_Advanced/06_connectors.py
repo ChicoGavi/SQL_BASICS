290331399640edf9d6671c4fa3990641b7943d2f
Santiago Gaviria <sgaviria@uc.edu.co>
2024-09-25 16:10:03 -0500

    Guardado

--- 06_Advanced/06_connectors.py
+++ 06_Advanced/06_connectors.py
@@ -5,41 +5,42 @@
 # Ejemplo de conexión desde Python a una base de datos local
 # Se ejemplifica cómo evitar SQL INJECTION
 
+# Instalacion paquete conector SQL: pip install mysql-connector-python
 
 import mysql.connector
 
+def mostrar_user(user):
 
-
-config = {
+        config = {
         "host": "127.0.0.1",
         "port": "3306",
         "database": "hola_mysql",
         "user": "root",
         "password": "admin1234"
-    }
+        }
 
-    # config = {
-    #     "host": "bpw0hq9h09e7mqicjhtl-mysql.services.clever-cloud.com",
-    #     "port": "3306",
-    #     "database": "bpw0hq9h09e7mqicjhtl",
-    #     "user": "uqzby88erlhvkrty",
-    #     "password": "oePXiCOHdU1WRV80NPyv"
-    # }
+         # config = {
+         #     "host": "bpw0hq9h09e7mqicjhtl-mysql.services.clever-cloud.com",
+        #     "port": "3306",
+         #     "database": "bpw0hq9h09e7mqicjhtl",
+         #     "user": "uqzby88erlhvkrty",
+         #     "password": "oePXiCOHdU1WRV80NPyv"
+         # }
 
-connection = mysql.connector.connect(**config)
-cursor = connection.cursor()
+        connection = mysql.connector.connect(**config)
+        cursor = connection.cursor()
 
-query = "SELECT * FROM users;"
-print(query)
-cursor.execute(query)
-result = cursor.fetchall()
+        query = "SELECT * FROM users WHERE user_id = 1 " 
+        print(query)
+        cursor.execute(query)
+        result = cursor.fetchall()
 
-for row in result:
-        print(row)
+        for row in result:
+                print(row)
 
-cursor.close()
-connection.close()
+        cursor.close()
+        connection.close()
 
 
-# print_user("Brais") 
-# print_user("'; UPDATE users SET age = '15' WHERE user_id = 1; --")
+        mostrar_user(1)
+        #mostrar_user("'; UPDATE users SET age = '15' WHERE user_id = 1; --")
